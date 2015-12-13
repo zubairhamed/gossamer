c5d7cdcb8943cbfefc19c02449f97d2b220941d0
Zubair Hamed <zubairhamed@gmail.com>
2015-12-13 22:54:42 +0800

    entities, utils test update

--- entities_test.go
+++ entities_test.go
@@ -1,1 +1,17 @@
 package gossamer
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEntities(t *testing.T) {
+	assert.Equal(t, ENTITY_THINGS, ThingEntity{}.GetType())
+	assert.Equal(t, ENTITY_LOCATIONS, LocationEntity{}.GetType())
+	assert.Equal(t, ENTITY_HISTORICALLOCATIONS, HistoricalLocationEntity{}.GetType())
+	assert.Equal(t, ENTITY_DATASTREAMS, DatastreamEntity{}.GetType())
+	assert.Equal(t, ENTITY_SENSORS, SensorEntity{}.GetType())
+	assert.Equal(t, ENTITY_OBSERVEDPROPERTIES, ObservedPropertyEntity{}.GetType())
+	assert.Equal(t, ENTITY_OBSERVATIONS, ObservationEntity{}.GetType())
+	assert.Equal(t, ENTITY_FEATURESOFINTERESTS, FeatureOfInterestEntity{}.GetType())
+}
--- utils_test.go
+++ utils_test.go
@@ -1,1 +1,11 @@
 package gossamer
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUtilFunctions(t *testing.T) {
+	assert.Equal(t, "Things(12345)", ResolveEntityLink("12345", ENTITY_THINGS))
+	assert.Equal(t, "http://localhost:8000/v1.0/Things(12345)", ResolveSelfLinkUrl("12345", "Things"))
+}
