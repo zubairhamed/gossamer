3c0cd84f5e3050b4e31a7200690dd451db9ab75b
Zubair Hamed <zubairhamed@gmail.com>
2015-12-13 22:15:50 +0800

    Test updates for resourcePath and resource path items

--- resourcepath_test.go
+++ resourcepath_test.go
@@ -11,6 +11,8 @@ func TestResourcePath(t *testing.T) {
 	var req Request
 	var err error
 	var rp ResourcePath
+	var prev ResourcePathItem
+	var next ResourcePathItem
 	var firstItem ResourcePathItem
 
 	url, _ = url.Parse("http://localhost:8000/v1.0/Things")
@@ -24,6 +26,34 @@ func TestResourcePath(t *testing.T) {
 	firstItem = rp.First()
 	assert.NotNil(t, firstItem)
 	assert.Equal(t, ENTITY_THINGS, firstItem.GetEntity())
+	assert.Equal(t, "", firstItem.GetId())
+	assert.Nil(t, firstItem.GetQueryOptions())
 
+	// http://localhost:8000/v1.0/Things(12345)
+	// http://localhost:8000/v1.0/Things(12345)/Locations
+	// http://localhost:8000/v1.0/Things(12345)/Locations(67890)
 
+	url, _ = url.Parse("http://localhost:8000/v1.0/Datastreams(12345)/Sensor")
+	req, err = CreateIncomingRequest(url, HTTP)
+	assert.Nil(t, err)
+	assert.NotNil(t, req)
+	rp = req.GetResourcePath()
+	assert.NotNil(t, rp)
+	assert.Equal(t, 2, len(rp.All()))
+	assert.Equal(t, -1, rp.CurrentIndex())
+	assert.Equal(t, "12345", rp.First().GetId())
+	assert.Equal(t, ENTITY_DATASTREAMS, rp.First().GetEntity())
+	assert.True(t, rp.IsFirst())
+	assert.NotNil(t, rp.Last())
+	assert.True(t, rp.IsLast())
+	assert.Equal(t, "", rp.Last().GetId())
+	assert.Equal(t, ENTITY_SENSOR, rp.Last().GetEntity())
+	prev = rp.Prev()
+	assert.Equal(t, "12345", prev.GetId())
+	assert.Equal(t, ENTITY_DATASTREAMS, prev.GetEntity())
+	next = rp.Next()
+	assert.Equal(t, "", next.GetId())
+	assert.Equal(t, ENTITY_SENSOR, next.GetEntity())
+	assert.False(t, rp.HasNext())
+	assert.Nil(t, rp.At(3))
 }
